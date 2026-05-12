29b526eec164ee428ad5e9235878e5e49da9b4ab
zayennn <zaayeennn@gmail.com>
2026-05-12 23:43:03 +0700

    update

--- 1. dasar php/1.1 apa itu php/index.php
+++ 1. dasar php/1.1 apa itu php/index.php
@@ -1,7 +1,19 @@
-<!--
-    Browser → Server → PHP diproses → HTML dikirim balik
--->
-
 <?php
+    /*
+        PHP itu bahasa backend
+
+        Artinya:
+        PHP jalan di server
+
+        Browser → Server → PHP diproses → HTML dikirim balik
+    */
+
     echo "hello world"
+
+    /*
+        Browser cuma nerima hasil akhirnya:
+        * hello world
+
+        Dia ga tau proses PHP-nya
+    */
 ?>
