6e49dc579d474416c72f58db793cbe4b99359dc3
zayennn <zaayeennn@gmail.com>
2026-05-13 00:22:47 +0700

    update

--- 1. dasar php/latihan/latihan 1.1/index.php
+++ 1. dasar php/latihan/latihan 1.1/index.php
@@ -9,5 +9,11 @@
         *Ganjil
     */
 
-    for ($i)
+    for ($i = 1; $i <= 10; $i++) {
+        if ($i % 2 == 0) {
+            echo "$i angka genap\n";
+        } else {
+            echo "$i angka ganjil\n";
+        }
+    }
 ?>
