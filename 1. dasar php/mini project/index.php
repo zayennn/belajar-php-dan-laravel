dccca2331de61eaae2a7b5a4bdb97a3136036fa3
zayennn <zaayeennn@gmail.com>
2026-05-13 00:11:48 +0700

    update

---
+++ 1. dasar php/mini project/index.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+        Bikin:
+            • "Aplikasi Biodata"
+
+        Fitur:
+            • input nama
+            • input umur
+            • tampilkan hasil
+            • kasih status dewasa / belum
+    */
+?>
