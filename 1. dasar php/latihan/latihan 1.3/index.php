308e4e61cad442d99af878ea4ae38f9466fb4427
zayennn <zaayeennn@gmail.com>
2026-05-13 11:57:05 +0700

    update

--- 1. dasar php/latihan/latihan 1.3/index.php
+++ 1. dasar php/latihan/latihan 1.3/index.php
@@ -1,11 +1,23 @@
 <?php
     $siswa = [
-        "nama" => "capypoter",
-        "umur" => 13,
-        "kelas" => 8
+        [
+            "nama" => "capypoter",
+            "umur" => 13,
+            "kelas" => 8
+        ],
+        [
+            "nama" => "capytanic",
+            "umur" => 12,
+            "kelas" => 7
+        ],
+        [
+            "nama" => "capypoter",
+            "umur" => 12,
+            "kelas" => 6
+        ]
     ];
 
-    for ($i = 0; $i < count($siswa); $i++) {
-        echo $siswa[$i];
+    for($i = 0; $i < count($siswa); $i++) {
+        echo "nama : $siswa[$i]";
     }
 ?>
