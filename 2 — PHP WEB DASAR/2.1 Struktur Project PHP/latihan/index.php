5d084deef955dea5224eb5bb2336a8f43089d94a
zayennn <zaayeennn@gmail.com>
2026-05-17 19:39:11 +0700

    update

--- 2 — PHP WEB DASAR/2.1 Struktur Project PHP/latihan/index.php
+++ 2 — PHP WEB DASAR/2.1 Struktur Project PHP/latihan/index.php
@@ -1,19 +1,27 @@
 <?php
-    $nama = "elang";
-    $umur = 19;
-    $hobi = "ngoding";
-    $cita_cita = "programmer";
-?>
 
-<!doctype html>
-<html>
-    <head>
-        <title>mini project</title>
-    </head>
-    <body>
-        <h1>nama        : <?= $nama ?></h1>
-        <h1>umur        : <?= $umur ?></h1>
-        <h1>hobi        : <?= $hobi ?></h1>
-        <h1>cita cita   : <?= $cita_cita ?></h1>
-    </body>
-</html>
+/*
+LATIHAN 2.1
+
+Buat halaman biodata sederhana.
+
+Tampilkan:
+
+- nama
+- umur
+- hobi
+- makanan favorit
+
+Gunakan:
+- variable PHP
+- echo HTML
+
+Contoh output:
+
+Nama: Elang
+Umur: 20
+Hobi: Ngoding
+Makanan favorit: Nasi Goreng
+*/
+
+?>
