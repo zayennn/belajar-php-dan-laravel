4de4ebaea4fa55931ae1113500c91c626e6775bb
zayennn <zaayeennn@gmail.com>
2026-05-17 18:43:35 +0700

    update

--- 1. dasar php/latihan/latihan 1.3/index.php
+++ 1. dasar php/latihan/latihan 1.3/index.php
@@ -20,9 +20,9 @@
     for($i = 0; $i < count($siswa); $i++) {
         echo "
 ========= siswa ke $i =========
-nama siswa  : {$siswa[$i]['nama']}
-umur siswa  : {$siswa[$i]['umur']}
-umur siswa  : {$siswa[$i]['umur']}
+nama siswa      : {$siswa[$i]['nama']}
+umur siswa      : {$siswa[$i]['umur']}
+kelas siswa     : {$siswa[$i]['kelas']}
         ";
     }
 ?>
