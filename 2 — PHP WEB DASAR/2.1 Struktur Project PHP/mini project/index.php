2ce44a8eda716191c59e6431126185bf74c986d9
zayennn <zaayeennn@gmail.com>
2026-05-17 19:45:10 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.1 Struktur Project PHP/mini project/index.php
@@ -0,0 +1,39 @@
+<?php
+
+    /*
+        MINI PROJECT 2.1
+
+        Bikin halaman profile sederhana.
+
+        Tampilkan:
+
+        - nama
+        - umur
+        - role
+        - cita-cita
+        - deskripsi singkat
+
+        Gunakan HTML + PHP.
+
+        Contoh tampilan:
+
+        ====================
+        PROFILE ELANG
+        ====================
+
+        Nama: Elang
+        Umur: 20
+        Role: Junior Fullstack Developer
+        Cita-cita: Software Engineer
+
+        Tentang Saya:
+        Saya suka ngoding web dan sedang belajar PHP.
+    */
+
+    $nama = "elang";
+    $umur = 19;
+    $role = "fullstack";
+    $cita_cita = "programmer";
+    $deskripsi = "Saya suka ngoding web dan sedang belajar PHP";
+?>
+
