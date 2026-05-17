3fffc1e86ba9cf82b0f633e0af7ed084cdfce874
zayennn <zaayeennn@gmail.com>
2026-05-17 20:02:26 +0700

    update

--- 2 — PHP WEB DASAR/2.2 HTML Di Dalam PHP/index1.php
+++ 2 — PHP WEB DASAR/2.2 HTML Di Dalam PHP/index1.php
@@ -1,3 +1,18 @@
 <?php
-    
+    $nama = "elang";
+    $umur = 19;
 ?>
+
+<!doctype html>
+<html>
+<head>
+    <title>Belajar PHP</title>
+</head>
+<body>
+
+    <h1>Halo <?= $nama ?></h1>
+
+    <p>Umur: <?= $umur ?></p>
+
+</body>
+</html>
