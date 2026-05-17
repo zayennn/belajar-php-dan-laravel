9dca02cb5a882f6ed3c1d3572a7b1806623da052
zayennn <zaayeennn@gmail.com>
2026-05-17 20:06:00 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.2 HTML Di Dalam PHP/latihan/index.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+LATIHAN 2.2
+
+Buat halaman "Profil Game"
+
+Tampilkan:
+
+- nama game
+- genre
+- developer
+- tahun rilis
+- alasan suka game itu
+
+Gunakan:
+
+- variable PHP
+- HTML
+- <?= ?>
+
+Contoh output:
+
+PROFIL GAME
+
+Nama Game : Valorant
+Genre : FPS
+Developer : Riot Games
+Tahun Rilis : 2020
+
+Kenapa Suka:
+Saya suka gameplay kompetitifnya.
+*/
+?>
