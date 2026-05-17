3679f7fc43206704698740006f561e71c386cf54
zayennn <zaayeennn@gmail.com>
2026-05-17 19:26:29 +0700

    update

--- 2 — PHP WEB DASAR/2.1 Struktur Project PHP/index2.php
+++ 2 — PHP WEB DASAR/2.1 Struktur Project PHP/index2.php
@@ -1,5 +1,9 @@
 <?php
     $nama = "elang";
     $umur = 19;
-    $role = "junior fullstack web developer"
+    $role = "junior fullstack web developer";
+
+    echo "<h1>halo $nama</h1>";
+    echo "<h1>umur  : $umur</h1>";
+    echo "<h1>halo  : $role</h1>";
 ?>
