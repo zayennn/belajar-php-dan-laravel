5072dcaf51b67bd406b60cf3c674cb84b18b0614
zayennn <zaayeennn@gmail.com>
2026-05-17 20:04:19 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.2 HTML Di Dalam PHP/index2.php
@@ -0,0 +1,24 @@
+<?php
+
+$nama = "Elang";
+$umur = 19;
+$hobi = "Ngoding";
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Biodata</title>
+    </head>
+    <body>
+
+        <h1>Biodata</h1>
+
+        <hr>
+
+        <p>Nama: <?= $nama ?></p>
+        <p>Umur: <?= $umur ?></p>
+        <p>Hobi: <?= $hobi ?></p>
+
+    </body>
+</html>
