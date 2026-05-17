e973b1f9b21ebb988d91b6a34cea5d954443f784
zayennn <zaayeennn@gmail.com>
2026-05-17 20:49:58 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.2 HTML Di Dalam PHP/mini project/index.php
@@ -0,0 +1,39 @@
+<?php
+
+    /*
+        MINI PROJECT 2.2
+
+        Buat halaman profile character game/anime/movie favorit.
+
+        Tampilkan:
+
+        - nama character
+        - asal game/anime/movie
+        - role
+        - kemampuan
+        - deskripsi singkat
+
+        Gunakan:
+
+        - HTML
+        - PHP variable
+        - <?= ?>
+        - heading
+        - paragraph
+        - hr
+
+        Contoh:
+
+        PROFILE CHARACTER
+
+        Nama: Vergil
+        Asal: Devil May Cry
+        Role: Anti Hero
+        Kemampuan: Yamato, teleport, judgement cut
+
+        Tentang Character:
+        Vergil adalah karakter dingin yang kuat.
+    */
+
+    
+?>
