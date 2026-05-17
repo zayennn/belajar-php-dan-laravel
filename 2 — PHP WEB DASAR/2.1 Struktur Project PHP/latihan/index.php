9e0e3e11746f0331283ee8ae928b5a6873c5b0fc
zayennn <zaayeennn@gmail.com>
2026-05-17 19:34:12 +0700

    update

--- 2 — PHP WEB DASAR/2.1 Struktur Project PHP/latihan/index.php
+++ 2 — PHP WEB DASAR/2.1 Struktur Project PHP/latihan/index.php
@@ -1,3 +1,19 @@
 <?php
-    
+    $nama = "elang";
+    $umur = 19;
+    $hobi = "ngoding";
+    $cita_cita = "programmer";
 ?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>mini project</title>
+    </head>
+    <body>
+        <h1>nama        : <?= $nama ?></h1>
+        <h1>umur        : <?= $umur ?></h1>
+        <h1>hobi        : <?= $hobi ?></h1>
+        <h1>cita cita   : <?= $cita_cita ?></h1>
+    </body>
+</html>
