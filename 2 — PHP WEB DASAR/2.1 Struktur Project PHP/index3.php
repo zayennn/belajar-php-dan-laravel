1e0439a7a2ebdd0996d5759aaca4371e697b04b7
zayennn <zaayeennn@gmail.com>
2026-05-17 19:28:32 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.1 Struktur Project PHP/index3.php
@@ -0,0 +1,15 @@
+<?php
+    $nama = "elang";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>belajar php</title>
+    </head>
+    <body>
+        <h1>
+            halo <?= $nama ?>
+        </h1>
+    </body>
+</html>
