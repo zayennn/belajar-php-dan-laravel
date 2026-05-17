48be06cf77e0ae726e82a4cc5bb4ac3259ddce3e
zayennn <zaayeennn@gmail.com>
2026-05-17 20:17:28 +0700

    update

--- 2 — PHP WEB DASAR/2.2 HTML Di Dalam PHP/latihan/index.php
+++ 2 — PHP WEB DASAR/2.2 HTML Di Dalam PHP/latihan/index.php
@@ -30,4 +30,31 @@
         Kenapa Suka:
         Saya suka gameplay kompetitifnya.
     */
+
+    $nama_game = "the elder scroll v: skyrim (anniversary edition)";
+    $genre = ["action", "adventure", "fantasy"];
+    $developer = "Bethesda Game Studios";
+    $tahun_rilis = 2011;
+    $alasan_suka = "karena dunia yang luas, bebas melakukan apa aja, bisa menjelajahi banyak dungeon, ada banyak jenis role untuk setiap npc/karakter pemain mulai dari vamapire, werewolves, archer, knight, wizard/witch, thief, dll. dan yang terpenting mempunyai komunitas mod yang besar dan itu di dukung oleh developer nya bahkan developer nya juga memberikan features creation untuk para penikmat game skyrim";
+
 ?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>latihan</title>
+    </head>
+    <body>
+        <h1>profil game</h1>
+        <hr>
+
+        <p>nama game : <?= $nama_game ?></p>
+        <p>genre : </p>
+        <ul>
+            <li></li>
+        </ul>
+        <p>nama game : <?= $nama_game ?></p>
+        <p>nama game : <?= $nama_game ?></p>
+        <p>nama game : <?= $nama_game ?></p>
+    </body>
+</html>
