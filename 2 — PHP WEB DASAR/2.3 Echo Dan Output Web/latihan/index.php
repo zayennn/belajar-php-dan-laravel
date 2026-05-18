0499e1717526e4c9a00af6418ace3cff08d132b2
zayennn <zaayeennn@gmail.com>
2026-05-18 08:06:54 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.3 Echo Dan Output Web/latihan/index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+LATIHAN 2.3
+
+Buat halaman "Profil Laptop"
+
+Tampilkan:
+
+- nama laptop
+- processor
+- RAM
+- VGA
+- alasan suka laptop itu
+
+Gunakan:
+
+- echo
+- variable PHP
+- double quote
+- concatenation (.)
+
+Minimal:
+1 bagian pakai interpolation
+1 bagian pakai concatenation
+
+Contoh output:
+
+PROFIL LAPTOP
+
+Nama Laptop: ASUS TUF
+Processor: Ryzen 7
+RAM: 16GB
+VGA: RTX 4060
+
+Kenapa suka:
+Karena performanya bagus.
+*/
