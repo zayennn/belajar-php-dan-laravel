9c8b2204051c0d2186a3814c59af5482504cd0d2
zayennn <zaayeennn@gmail.com>
2026-05-18 08:18:20 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.3 Echo Dan Output Web/mini project/index.php
@@ -0,0 +1,31 @@
+<?php
+
+    /*
+        MINI PROJECT 2.3
+
+        Buat halaman profile setup gaming/ngoding kamu.
+
+        Tampilkan:
+
+        - nama setup
+        - monitor
+        - keyboard
+        - mouse
+        - processor
+        - RAM
+        - deskripsi setup
+
+        Gunakan:
+
+        - echo
+        - interpolation
+        - concatenation
+        - HTML output
+
+        Minimal:
+        - ada <h1>
+        - ada <p>
+        - ada <hr>
+
+        Semua output pakai echo.
+    */
