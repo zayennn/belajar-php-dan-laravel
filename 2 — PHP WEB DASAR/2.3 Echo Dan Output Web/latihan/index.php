49296eed34a15772f2b68feebef4d8c5dc93059c
zayennn <zaayeennn@gmail.com>
2026-05-18 08:07:18 +0700

    update

--- 2 — PHP WEB DASAR/2.3 Echo Dan Output Web/latihan/index.php
+++ 2 — PHP WEB DASAR/2.3 Echo Dan Output Web/latihan/index.php
@@ -1,38 +1,39 @@
 <?php
 
-/*
-LATIHAN 2.3
+    /*
+        LATIHAN 2.3
 
-Buat halaman "Profil Laptop"
+        Buat halaman "Profil Laptop"
 
-Tampilkan:
+        Tampilkan:
 
-- nama laptop
-- processor
-- RAM
-- VGA
-- alasan suka laptop itu
+        - nama laptop
+        - processor
+        - RAM
+        - VGA
+        - alasan suka laptop itu
 
-Gunakan:
+        Gunakan:
 
-- echo
-- variable PHP
-- double quote
-- concatenation (.)
+        - echo
+        - variable PHP
+        - double quote
+        - concatenation (.)
 
-Minimal:
-1 bagian pakai interpolation
-1 bagian pakai concatenation
+        Minimal:
+        1 bagian pakai interpolation
+        1 bagian pakai concatenation
 
-Contoh output:
+        Contoh output:
 
-PROFIL LAPTOP
+        PROFIL LAPTOP
 
-Nama Laptop: ASUS TUF
-Processor: Ryzen 7
-RAM: 16GB
-VGA: RTX 4060
+        Nama Laptop: ASUS TUF
+        Processor: Ryzen 7
+        RAM: 16GB
+        VGA: RTX 4060
 
-Kenapa suka:
-Karena performanya bagus.
-*/
+        Kenapa suka:
+        Karena performanya bagus.
+    */
+
