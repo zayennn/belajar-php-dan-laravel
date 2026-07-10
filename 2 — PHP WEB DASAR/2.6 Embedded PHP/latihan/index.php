ed81a424837a0c342778208d210ba560ec9c9651
zayennn <zaayeennn@gmail.com>
2026-07-10 15:24:27 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.6 Embedded PHP/latihan/index.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+====================================================
+
+LATIHAN 2.6
+
+====================================================
+
+Buat halaman "Profil Mobil"
+
+Data:
+
+- merk
+- tipe
+- mesin
+- tenaga
+- harga
+
+Gunakan:
+
+- Embedded PHP
+- <?= ?>
+- HTML
+- Jangan gunakan echo untuk menampilkan HTML.
+
+BONUS
+
+Harga tampil menggunakan number_format().
+
+====================================================
+*/
+
+$merk = "";
+$tipe = "";
+$mesin = "";
+$tenaga = "";
+$harga = 0;
+?>
