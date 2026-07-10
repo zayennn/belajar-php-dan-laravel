6d7983843f1bd0073857005409b4dd4b311222d9
zayennn <zaayeennn@gmail.com>
2026-07-10 14:15:48 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.4 Variable Di Web/mini project/index.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+MINI PROJECT 2.4
+
+Bikin halaman "Setup Warnet Gaming"
+
+Tampilkan:
+
+- nama warnet
+- jumlah PC
+- harga per jam
+- total pendapatan jika semua PC dipakai 5 jam
+
+Gunakan:
+
+- variable
+- operasi matematika
+- output HTML
+- echo
+
+Minimal:
+- ada <h1>
+- ada <p>
+- ada <hr>
+
+Contoh:
+
+WARNET GAMING
+
+Nama Warnet: CapyNet
+Jumlah PC: 20
+Harga per jam: 5000
+
+Jika penuh 5 jam:
+
+Total pendapatan:
+500000
+*/
