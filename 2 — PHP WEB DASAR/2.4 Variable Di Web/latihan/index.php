a048d1892bd62e48fa187a277761f54c6e37f62a
zayennn <zaayeennn@gmail.com>
2026-07-10 14:06:32 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.4 Variable Di Web/latihan/index.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+LATIHAN 2.4
+
+Buat halaman "Profil HP"
+
+Tampilkan:
+
+- nama HP
+- chipset
+- RAM
+- storage
+- harga
+
+Lalu hitung:
+
+kalau beli 3 HP
+berapa total harganya
+
+Gunakan:
+
+- variable
+- operasi variable
+- output web
+
+Contoh:
+
+Nama HP: iPhone
+Harga: 10000000
+
+Jumlah beli: 3
+
+Total:
+30000000
+*/
