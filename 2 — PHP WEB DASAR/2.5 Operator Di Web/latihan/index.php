4a45d42638377257fffabb71edaee47220d41dd6
zayennn <zaayeennn@gmail.com>
2026-07-10 14:50:58 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.5 Operator Di Web/latihan/index.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+=========================================
+
+LATIHAN 2.5
+
+=========================================
+
+Buat halaman "Kasir Sederhana"
+
+Data:
+
+Nama Barang
+Harga Barang
+Jumlah Beli
+
+Hitung:
+
+Subtotal
+
+Lalu tampilkan:
+
+Nama Barang
+Harga
+Jumlah
+Subtotal
+
+Gunakan:
+
+- operator aritmatika
+- assignment
+- interpolation
+- concatenation
+- number_format()
+
+=========================================
+
+BONUS
+
+Tambahkan:
+
+Diskon Rp25.000
+
+Gunakan:
+
+-=
+
+Contoh:
+
+$total -= 25000;
+
+=========================================
+*/
