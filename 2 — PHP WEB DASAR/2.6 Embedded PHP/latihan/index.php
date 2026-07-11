35c07e74a58a01c3f8421bd35b2af8a99421f849
zayennn <zaayeennn@gmail.com>
2026-07-11 10:33:12 +0700

    update

--- 2 — PHP WEB DASAR/2.6 Embedded PHP/latihan/index.php
+++ 2 — PHP WEB DASAR/2.6 Embedded PHP/latihan/index.php
@@ -38,3 +38,14 @@ $tenaga = "431 hp";
 $harga = 3490000000;
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
