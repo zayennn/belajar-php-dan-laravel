acff9423732870e1b1e957602f8e5a7300337658
zayennn <zaayeennn@gmail.com>
2026-07-11 11:14:05 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.7 Percabangan Di Web/mini-project/index.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+===================================================
+
+MINI PROJECT 2.7
+
+===================================================
+
+Buat halaman "Dashboard User"
+
+Data:
+
+Nama
+Role
+
+Role bisa:
+
+Admin
+Member
+
+Jika:
+
+Admin
+
+Tampilkan:
+
+Panel Admin
+Kelola User
+Kelola Produk
+
+Jika:
+
+Member
+
+Tampilkan:
+
+Dashboard Member
+Riwayat Pembelian
+Edit Profile
+
+Gunakan:
+
+- if else
+- Alternative Syntax
+- HTML
+
+BONUS
+
+Tambahkan:
+
+Status Login
+
+Jika login
+
+Tampilkan:
+
+"Selamat Datang"
+
+Jika tidak
+
+"Tolong Login"
+
+Gunakan boolean.
+
+===================================================
+*/
+
+$nama = "";
+$role = "";
+$isLogin = true;
+
+?>
