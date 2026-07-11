fd6161eb0ef20e0f8cef06f0f1a612f0263a9be0
zayennn <zaayeennn@gmail.com>
2026-07-11 11:13:55 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.7 Percabangan Di Web/latihan/index.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+===================================================
+
+LATIHAN 2.7
+
+===================================================
+
+Buat halaman "Status Kelulusan"
+
+Data:
+
+Nama
+Nilai
+
+Aturan:
+
+>= 75
+
+"LULUS"
+
+Selain itu
+
+"TIDAK LULUS"
+
+Gunakan:
+
+- if else
+- Embedded PHP
+- Alternative Syntax
+
+BONUS
+
+Kalau nilai >= 90
+
+Tambahkan tulisan:
+
+"Selamat! Nilai Sangat Baik"
+
+===================================================
+*/
+
+$nama = "";
+$nilai = 0;
+
+?>
