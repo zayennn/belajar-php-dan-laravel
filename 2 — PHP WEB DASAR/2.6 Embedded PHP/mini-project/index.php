9101db7ae85e5b30c8ebfda27bf151ec5db83273
zayennn <zaayeennn@gmail.com>
2026-07-11 10:39:09 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.6 Embedded PHP/mini-project/index.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+====================================================
+
+MINI PROJECT 2.6
+
+====================================================
+
+Buat halaman "Profil PC Impian"
+
+Data:
+
+- Nama PC
+- Processor
+- VGA
+- RAM
+- SSD
+- Monitor
+- Estimasi Harga
+
+Gunakan:
+
+- Embedded PHP
+- HTML
+- <?= ?>
+
+Tidak boleh memakai:
+
+echo "<h1>...."
+
+Semua HTML harus ditulis langsung.
+
+BONUS
+
+Tambahkan:
+
+Daftar Aksesoris
+
+Gunakan array.
+
+Tampilkan memakai:
+
+foreach
+
+dengan Alternative Syntax.
+
+====================================================
+*/
+
+$namaPc = "";
+$processor = "";
+$vga = "";
+$ram = "";
+$ssd = "";
+$monitor = "";
+$harga = 0;
+
+$aksesoris = [];
+?>
