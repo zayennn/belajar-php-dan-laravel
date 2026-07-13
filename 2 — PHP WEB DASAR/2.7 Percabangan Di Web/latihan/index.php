f34673befcd84f1295e13b069de757c1c1b85dd5
zayennn <zaayeennn@gmail.com>
2026-07-13 22:11:39 +0700

    update

--- 2 — PHP WEB DASAR/2.7 Percabangan Di Web/latihan/index.php
+++ 2 — PHP WEB DASAR/2.7 Percabangan Di Web/latihan/index.php
@@ -41,7 +41,19 @@ Tambahkan tulisan:
 ===================================================
 */
 
-$nama = "";
-$nilai = 0;
+$nama = "Capypoter";
+$nilai = 100;
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
