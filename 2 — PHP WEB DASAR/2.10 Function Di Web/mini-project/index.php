ea3b5cbb17ef7f1faca962383ffd6672b6f95fd9
zayennn <zaayeennn@gmail.com>
2026-07-14 01:09:26 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.10 Function Di Web/mini-project/index.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+====================================================
+
+MINI PROJECT 2.10
+
+====================================================
+
+Buat function:
+
+movieCard()
+
+Parameter:
+
+Judul
+Genre
+Rating
+Harga
+
+Function menampilkan card HTML.
+
+Panggil minimal 5 kali.
+
+BONUS
+
+Jika rating >= 8
+
+Badge:
+
+Recommended
+
+Selain itu
+
+Normal
+
+Gunakan if.
+
+====================================================
+*/
+
+?>
