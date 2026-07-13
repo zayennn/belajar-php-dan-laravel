baf7d04a950fab6e0edc00d53351c10b5e9db7c4
zayennn <zaayeennn@gmail.com>
2026-07-14 00:16:34 +0700

    update

--- 2 — PHP WEB DASAR/2.9 Looping Di Web/latihan/index.php
+++ 2 — PHP WEB DASAR/2.9 Looping Di Web/latihan/index.php
@@ -46,3 +46,17 @@ Selain itu
 $movies = [];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Daftar film</h1>
+    <hr>
+    
+</body>
+</html>
