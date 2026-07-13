74f56c43763515f6cf6b8fe151590016313f0d5b
zayennn <zaayeennn@gmail.com>
2026-07-14 00:20:53 +0700

    update

--- 2 — PHP WEB DASAR/2.9 Looping Di Web/latihan/index.php
+++ 2 — PHP WEB DASAR/2.9 Looping Di Web/latihan/index.php
@@ -89,5 +89,25 @@ $movies = [
     <h1>Daftar film</h1>
     <hr>
 
+    <table border="1">
+        <thead>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
 </body>
 </html>
