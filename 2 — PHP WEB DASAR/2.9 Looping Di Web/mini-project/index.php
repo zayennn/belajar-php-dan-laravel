bff329fb6aa516db81b75f5bd6bc9ce91d797726
zayennn <zaayeennn@gmail.com>
2026-07-14 00:15:26 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.9 Looping Di Web/mini-project/index.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+=====================================================
+
+MINI PROJECT 2.9
+
+=====================================================
+
+Buat halaman "Daftar Mahasiswa"
+
+Gunakan array multidimensi.
+
+Minimal 5 mahasiswa.
+
+Data:
+
+Nama
+Kelas
+Nilai
+
+Gunakan:
+
+foreach
+
+Tampilkan ke table.
+
+BONUS
+
+Tambahkan:
+
+Grade
+
+A
+
+B
+
+C
+
+D
+
+Menggunakan if elseif.
+
+=====================================================
+*/
+
+$students = [];
+
+?>
