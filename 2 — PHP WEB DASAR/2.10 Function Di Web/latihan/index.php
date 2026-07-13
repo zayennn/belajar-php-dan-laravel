31f9af734fa1d4886a4fa1d897679516608b4e8b
zayennn <zaayeennn@gmail.com>
2026-07-14 01:09:15 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.10 Function Di Web/latihan/index.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+====================================================
+
+LATIHAN 2.10
+
+====================================================
+
+Buat function:
+
+profileCard()
+
+Parameter:
+
+Nama
+Umur
+Role
+
+Function harus menampilkan:
+
+------------------
+
+Nama
+
+Umur
+
+Role
+
+------------------
+
+Gunakan HTML.
+
+Panggil minimal 3 kali.
+
+BONUS
+
+Tambahkan:
+
+Status
+
+Jika umur >=17
+
+"Dewasa"
+
+Selain itu
+
+"Belum Dewasa"
+
+====================================================
+*/
+
+?>
