9cbd6b57dbefc4befb9a7c8ea5b47a84d98121f3
zayennn <zaayeennn@gmail.com>
2026-07-13 22:21:38 +0700

    update

--- 2 — PHP WEB DASAR/2.7 Percabangan Di Web/mini-project/index.php
+++ 2 — PHP WEB DASAR/2.7 Percabangan Di Web/mini-project/index.php
@@ -72,3 +72,17 @@ $isLogin = true;
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Dashboard user</h1>
+    <hr>
+
+    
+</body>
+</html>
