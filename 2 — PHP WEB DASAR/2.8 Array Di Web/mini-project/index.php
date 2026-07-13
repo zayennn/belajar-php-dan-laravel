5216a200a9b36926f868375f302ab0436dcee145
zayennn <zaayeennn@gmail.com>
2026-07-13 23:22:20 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.8 Array Di Web/mini-project/index.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+====================================================
+
+MINI PROJECT 2.8
+
+====================================================
+
+Buat halaman "Daftar Game"
+
+Gunakan:
+
+Array Multidimensi.
+
+Minimal ada 3 game.
+
+Setiap game memiliki:
+
+Nama
+Genre
+Developer
+Harga
+
+Belum menggunakan loop.
+
+Tampilkan satu-satu secara manual.
+
+Bonus:
+
+Tambahkan:
+
+Rating
+
+====================================================
+*/
+
+$games = [];
+
+?>
