559ef8e60b48029a22dc2a8c1f1e414fe3e4fbc3
zayennn <zaayeennn@gmail.com>
2026-07-13 23:21:54 +0700

    update

---
+++ 2 — PHP WEB DASAR/2.8 Array Di Web/latihan/index.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+====================================================
+
+LATIHAN 2.8
+
+====================================================
+
+Buat halaman "Profil Smartphone"
+
+Gunakan Associative Array.
+
+Data:
+
+Nama
+Chipset
+RAM
+Storage
+Harga
+
+Tampilkan semuanya ke HTML.
+
+Gunakan:
+
+<?= ?>
+
+Bonus:
+
+Tambahkan
+
+Warna
+
+====================================================
+*/
+
+$phone = [];
+
+?>
