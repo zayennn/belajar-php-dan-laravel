5af8182587dacdc47053fd8f4811b7a96ae9dc9a
zayennn <zaayeennn@gmail.com>
2026-07-13 23:27:22 +0700

    update

--- 2 — PHP WEB DASAR/2.8 Array Di Web/latihan/index.php
+++ 2 — PHP WEB DASAR/2.8 Array Di Web/latihan/index.php
@@ -34,6 +34,28 @@ Warna
 ====================================================
 */
 
-$phone = [];
+$phone = [
+    "nama" => "ROG Phone 9 Pro",
+    "layar" => "6.78 inci LTPO AMOLED FHD+ (2400 x 1080 piksel), refresh rate adaptif hingga 185Hz, kecerahan puncak 2500 nits",
+    "chipset" => " Qualcomm Snapdragon 8 Elite (Octa-core hingga 4.3 GHz) dengan GPU Adreno 830",
+    "ramDanPenyimpanan" => "LPDDR5X + UFS 4.0. Tersedia dalam varian 16GB/512GB dan 24GB/1TB (Pro Edition)",
+    "kameraBelakang" => "Utama 50 MP, Ultrawide 13 MP, dan Telefoto",
+    "kameraDepan" => "32 MP",
+    "bateraiDanPengisianDaya" => "5800mAh, fast charging 65W (serta pengisian nirkabel 15W)",
+    "sistemOperasi" => "Android 15 dengan ROG UI",
+    "fiturLainnya" => "Sensor AirTrigger, WiFi 7, NFC, dan lampu RGB"
+];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
