d2e07e03b9f4c729c047c5cb96240ae92ba9f6bb
zayennn <zaayeennn@gmail.com>
2026-07-13 23:28:16 +0700

    update

--- 2 — PHP WEB DASAR/2.8 Array Di Web/latihan/index.php
+++ 2 — PHP WEB DASAR/2.8 Array Di Web/latihan/index.php
@@ -41,6 +41,7 @@ $phone = [
     "ramDanPenyimpanan" => "LPDDR5X + UFS 4.0. Tersedia dalam varian 16GB/512GB dan 24GB/1TB (Pro Edition)",
     "kameraBelakang" => "Utama 50 MP, Ultrawide 13 MP, dan Telefoto",
     "kameraDepan" => "32 MP",
+    "warna" => "Hitam",
     "bateraiDanPengisianDaya" => "5800mAh, fast charging 65W (serta pengisian nirkabel 15W)",
     "sistemOperasi" => "Android 15 dengan ROG UI",
     "fiturLainnya" => "Sensor AirTrigger, WiFi 7, NFC, dan lampu RGB"
@@ -59,6 +60,6 @@ $phone = [
     <h1>Profil Smartphone</h1>
     <hr>
 
-    
+
 </body>
 </html>
